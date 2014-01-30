4f722f8bd95254bbaca84f206caf28e26750b553
Darby Frey <darbyfrey@gmail.com>
2014-01-30 09:08:31 -0600

    Migrating theme files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,34 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require vendor/jquery191
+//= require vendor/jqueryui1102
+//= require vendor/bootstrap/bootstrap
+//= require vendor/bootstrap-datepicker
+//= require vendor/fullcalendar/fullcalendar
+//= require vendor/jquery_gritter
+//= require vendor/sparkline
+//= require vendor/tags_input
+//= require vendor/jquery_uniform
+//= require vendor/jquery.icheck
+//= require vendor/select2
+//= require vendor/d3
+//= require vendor/xcharts
+//= require vendor/bootstrap-wysihtml5
+//= require vendor/justgage
+//= require vendor/wizard/bootstrap-wizard
+//= require vendor/wizard/wizard_demo
+//= require vendor/jquery.easy-pie-chart
+//= require vendor/touchTouch
+//= require vendor/dataTables
+//= require vendor/responsive-tables
+//= require vendor/validation/jquery.validationEngine-en
+//= require vendor/validation/jquery.validationEngine
+//= require vendor/fullcalendar/calendar_events
+//= require vendor/bootstrap-editable
+//= require vendor/editable/editable_demo
+//= require vendor/ibutton
+//= require gritter
+//= require calendar
+//= require charts/xcharts_left_sine
+//= require charts/xcharts_sine
+//= require charts/xcharts_bar
+//= require charts/sparkline_samples
+//= require generic
