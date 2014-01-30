359225b2e8d874fd234f39a1619c0fb91e1ac665
Darby Frey <darbyfrey@gmail.com>
2014-01-30 07:47:59 -0600

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ExecDash::Application.config.secret_key_base = 'b6c5f331293b54f26dba53375b6e4059bbbd21384b135c41892a247da4249238f00aa2654a5ab105bdf7c81fba125f1caaeab24fa9b17a7ab421f0da1a37f903'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ExecDash::Application.config.session_store :cookie_store, key: '_exec_dash_session'
