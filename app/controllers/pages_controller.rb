9547d9d12e881c006785dcc2efdb9fe586ebd7ee
Darby Frey <darbyfrey@gmail.com>
2014-01-30 09:08:48 -0600

    Adding routes and pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,48 @@
+class PagesController < ApplicationController
+
+  def root
+    redirect_to dashboard_pages_path
+  end
+
+  def dashboard
+  end
+
+  def buttons
+  end
+
+  def general
+  end
+
+  def icons
+  end
+
+  def grid
+  end
+
+  def tables
+  end
+
+  def widgets
+  end
+
+  def forms
+  end
+
+  def charts
+  end
+
+  def wizard
+  end
+
+  def login
+  end
+
+  def sign_up
+  end
+
+  def full_calendar
+  end
+
+  def error404
+  end
+end
