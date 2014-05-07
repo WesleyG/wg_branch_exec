24931c8f8a30f17b5ba7a9e9c403ebd08b82319d
WesleyG <wes.gillis@gmail.com>
2014-05-06 21:29:59 -0700

    static html pages - 6 tests passing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,11 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+    end
   end
 
   describe "Help page" do
@@ -15,6 +20,11 @@ describe "Static pages" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
@@ -24,15 +34,10 @@ describe "Static pages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
-  end
 
-  describe "Dashboard page" do
-
-    it "should have the content 'Performance by State'" do
-      visit '/demo/dashboard/dashboard.html'
-      expect(page).to have_content('Performance by State')
-      expect(page).to have_content('Churn Report')
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
     end
   end
-
 end
