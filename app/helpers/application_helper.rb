6ecf1ce7303ea62fe1e7c4174c860a6af1c66e09
WesleyG <wes.gillis@gmail.com>
2014-05-07 20:58:41 -0700

    up to 4.3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -126,13 +126,13 @@ module ApplicationHelper
 =end
 
 # WGG 05/07/2014 - added for rails tutorial/static pages
-  # Returns the full title on a per-page basis.
-  def full_title(page_title)
-    base_title = "Ruby on Rails Tutorial Sample App"
-    if page_title.empty?
-      base_title
+  # Returns the full title on a per-page basis.       
+  def full_title(page_title)                          # Method definition
+    base_title = "Ruby on Rails Tutorial Sample App"  # Variable assignment
+    if page_title.empty?                              # Boolean test
+      base_title                                      # Implicit return
     else
-      "#{base_title} | #{page_title}"
+      "#{base_title} | #{page_title}"                 # String interpolation
     end
   end
 
