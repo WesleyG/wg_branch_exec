53c6c3baf2fe6581573192513063033de3af1697
WesleyG <wes.gillis@gmail.com>
2014-05-07 20:33:27 -0700

    up to 4.2

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -125,4 +125,15 @@ module ApplicationHelper
   end
 =end
 
+# WGG 05/07/2014 - added for rails tutorial/static pages
+  # Returns the full title on a per-page basis.
+  def full_title(page_title)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
 end
