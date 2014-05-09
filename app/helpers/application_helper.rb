e75190c208ecc6816dd6f4b0d67bb1b4e86bcfaa
WesleyG <wes.gillis@gmail.com>
2014-05-08 21:22:28 -0700

    evolve page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -128,7 +128,7 @@ module ApplicationHelper
 # WGG 05/07/2014 - added for rails tutorial/static pages
   # Returns the full title on a per-page basis.       
   def full_title(page_title)                          # Method definition
-    base_title = "Ruby on Rails Tutorial Sample App"  # Variable assignment
+    base_title = "Evolve Exec"  # Variable assignment
     if page_title.empty?                              # Boolean test
       base_title                                      # Implicit return
     else
