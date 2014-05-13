e64f799196c585e5c55c91c4ee7e6c7df955d38f
WesleyG <wes.gillis@gmail.com>
2014-05-12 19:22:08 -0700

    regex for valid email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   validates :name,  presence: true, length: { maximum: 50 } # also valid -- validates(:name, presence: true)
-  validates :email, presence: true
-  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
