baf16c7f7700b59d3588f4649d6b64bc7ff36c38
WesleyG <wes.gillis@gmail.com>
2014-05-12 19:15:18 -0700

    test for user name length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :name,  presence: true # also valid -- validates(:name, presence: true)
+  validates :name,  presence: true, length: { maximum: 50 } # also valid -- validates(:name, presence: true)
   validates :email, presence: true
   
 end
