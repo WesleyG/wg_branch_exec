6af816e49dabaf20a90e18637025c2cb0e1d046e
WesleyG <wes.gillis@gmail.com>
2014-05-18 15:13:17 -0700

    tests passing

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -18,11 +18,11 @@ describe "Authentication" do
       before { click_button "Sign in" }
 
       it { should have_title('Sign in') }
-      it { should have_selector('div.alert.alert-error') }
-
+      it { should have_content('Invalid email/password combination') }
+ 
 			describe "after visiting another page" do
 				before { click_link "Home" }
-				it { should_not have_selector('div.alert.alert-error') }
+				it { should have_content('Invalid email/password combination') }
       end
     end
 
@@ -35,7 +35,6 @@ describe "Authentication" do
       end
 
       it { should have_title(user.name) }
-      it { should have_link('Profile',     href: user_path(user)) }
       it { should have_link('Sign out',    href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
    
