41ceb507113b9da4c86c24591f26913ee44b9f44
WesleyG <wes.gillis@gmail.com>
2014-05-17 19:38:03 -0700

    add signin page and tests

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+
+  # This removes the default layout set by application_controller.rb
+  layout 'custom.html.erb'
+  
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+  
+end
